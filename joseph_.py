81f9828bd51a79cd096e86dfae3d608c91c2a3a1
Joe <92016024+bagolybagoly69@users.noreply.github.com>
2022-03-31 14:29:29 +0200

    Add files via upload

---
+++ joseph_.py
@@ -0,0 +1,47 @@
+f=open("nevezetessegek.txt","r",encoding="utf8")
+nagylista=[]
+for sor in f:
+    kislista=sor[:-1].split("-")
+    nagylista.append(kislista)
+f.close()
+#évszámos
+def asd12():
+    bemenet=int(input("Adj meg egy évszámot: "))
+    for i in range(0,len(nagylista)):
+        if bemenet==int(nagylista[i][1]):
+            print(nagylista[i][0])
+#országos
+def asd13():
+    bemenet=input("Adj meg egy orszagot: ")
+    for i in range(0,len(nagylista)):
+        if bemenet==nagylista[i][0]:
+            print(nagylista[i][1])
+#nevezetességes
+def asd14():
+    for i in range(0,len(nagylista)):
+        print(nagylista[i][2])
+    nevezetes=input("kérlek add meg a nevezetesség nevét")
+    for i in range(0,len(nagylista)):
+        if nevezetes==nagylista[i][2]:
+            print(nagylista[i][0])
+            print(nagylista[i][1])
+
+#menu rendszer
+print("1. menupont","2. menupont","3. menupont")
+while True:
+    try:
+        if választás==1:
+            asd12()
+            break
+        if választás==2:
+            asd13()
+            break
+        if választás==3:
+            asd14()
+            break
+        else:
+            raise ValueError
+    except:
+        ValueError
+        print("Nincs ilyen")
+        
